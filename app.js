f496ba671b38393684bf09b1a1dad4e42c461755
leinonenko <Kabaman1995p>
2021-11-14 17:23:14 +0100

    installed express-session and cookie-parser

--- app.js
+++ app.js
@@ -2,11 +2,15 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const cookieParser = require('cookie-parser');
 
+
+app.use(cookieParser());
 app.set('views', './views');
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
+  console.log('cookies', req.cookies);
   res.render('home');
 });
 
