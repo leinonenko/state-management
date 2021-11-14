9ec8d565fdd814f0eea1db7f0007258bfe583173
leinonenko <Kabaman1995p>
2021-11-14 18:06:03 +0100

    Cookie

--- app.js
+++ app.js
@@ -3,15 +3,42 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 
-app.use(cookieParser());
+const username = 'foo';
+const password = 'bar';
+
 app.set('views', './views');
 app.set('view engine', 'pug');
 
+app.use(cookieParser());
+app.use(express.urlencoded({extended: true}));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+}));
+
 app.get('/', (req, res) => {
   console.log('cookies', req.cookies);
+  console.log('session', req.session);
   res.render('home');
+});
+
+app.get('/setCookie/:clr', (req,res)=>{
+  res.cookie('color', req.params.clr);
+  res.send('cookie set');
+});
+
+app.get('/getCookie', (req, res) => {
+  console.log('cookies', req.cookies);
+  res.send(color is ${req.cookies.color});
+});
+app.get('/deleteCookie', (req, res) => {
+  res.clearCookie('color');
+  console.log('cookies', req.cookies);
+  res.send('delete cookie color');
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
